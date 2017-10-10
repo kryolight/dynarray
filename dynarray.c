a4036e0210159ba6b496f688f249e5d26dafdec8
Rob Hess <robwhess@gmail.com>
2017-10-09 17:00:03 -0700

    Add code from 10/9 lecture.

--- dynarray.c
+++ dynarray.c
@@ -12,16 +12,15 @@ struct dynarray {
   int capacity;
 };
 
-struct dynarray* dynarray_create(int capacity) {
+struct dynarray* dynarray_create() {
 
-  assert(capacity > 0);
   struct dynarray* da = malloc(sizeof(struct dynarray));
   assert(da);
 
-  da->data = malloc(capacity * sizeof(int));
+  da->data = malloc(DYNARRAY_INIT_CAPACITY * sizeof(int));
   assert(da->data);
   da->size = 0;
-  da->capacity = capacity;
+  da->capacity = DYNARRAY_INIT_CAPACITY;
 
   return da;
 
@@ -70,9 +69,32 @@ void dynarray_set(struct dynarray* da, int idx, int val) {
 
 }
 
+/*
+ * Auxilliary function to perform a resize on the underlying array.
+ */
 void _dynarray_resize(struct dynarray* da, int new_capacity) {
 
   assert(new_capacity > da->size);
+
+  /*
+   * Allocate space for the new array.
+   */
+  int* new_data = malloc(new_capacity * sizeof(int));
+  assert(new_data);
+
+  /*
+   * Copy data from the old array to the new one.
+   */
+  for (int i = 0; i < da->size; i++) {
+    new_data[i] = da->data[i];
+  }
+
+  /*
+   * Put the new array into the dynarray struct.
+   */
+  free(da->data);
+  da->data = new_data;
+  da->capacity = new_capacity;
 
 }
 
@@ -80,6 +102,33 @@ void dynarray_insert(struct dynarray* da, int idx, int val) {
 
   assert(da);
   assert((idx < da->size && idx >= 0) || idx == -1);
+
+  // Let users specify idx = -1 to indicate the end of the array.
+  if (idx == -1) {
+    idx = da->size;
+  }
+
+  /*
+   * Make sure we have enough space for the new element.
+   */
+  if (da->size == da->capacity) {
+    printf("== Increasing DA capacity to %d\n", 2 * da->capacity);
+    _dynarray_resize(da, 2 * da->capacity);
+  }
+
+  /*
+   * Move all elements behind the new one back one index to make space
+   * for the new one.
+   */
+  for (int i = da->size; i > idx; i--) {
+    da->data[i] = da->data[i-1];
+  }
+
+  /*
+   * Put the new element into the array.
+   */
+  da->data[idx] = val;
+  da->size++;
 
 }
 
@@ -92,5 +141,15 @@ void dynarray_remove(struct dynarray* da, int idx) {
   if (idx == -1) {
     idx = da->size - 1;
   }
+
+  /*
+   * Move all elements behind the one being removed forward one index,
+   * overwriting the element to be removed in the process.
+   */
+  for (int i = idx; i < da->size - 1; i++) {
+    da->data[i] = da->data[i+1];
+  }
+
+  da->size--;
 
 }
